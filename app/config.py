8fc010699a1d419b54afce3a7be4f46ece389d71
Aleksey Loktionov <sams12.34.58@gmail.com>
2025-11-30 19:20:21 +0500

    delete bot_token

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
 ADMIN_CHAT_ID="1734746875"
-BOT_TOKEN = '8283387749:AAF3oHC9zhU1uCEluTw9Gfvx6zVJeX_r9us'
-# with open('hash.txt', 'r', encoding='utf-8') as file:
-#     BOT_TOKEN = file.read()
+with open('hash.txt', 'r', encoding='utf-8') as file:
+    BOT_TOKEN = file.read()
